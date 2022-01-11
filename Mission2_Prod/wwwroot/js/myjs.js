2667ae077623c0140a54c0ab39952aa0f8fdec7a
ytagrii <72105289+ytagrii@users.noreply.github.com>
2022-01-10 20:38:15 -0700

    changes

---
+++ Mission2_Prod/wwwroot/js/myjs.js
@@ -0,0 +1,24 @@
+﻿$("#calGrade").click(function () {
+    let assigGrade = $("#assigScore").val();
+    let assigQuiz = $("#quizScore").val();
+    let assigExam = $("#examScore").val();
+    let assigIntex = $("#intexScore").val();
+    let assigGroup = $("#groupScore").val();
+
+    let finalScore = assigGrade * 0.55;
+    finalScore += assigQuiz * 0.10;
+    finalScore += assigExam * 0.20;
+    finalScore += assigIntex * 0.10;
+    finalScore += assigGroup * 0.05;
+
+    document.getElementById("theForm").style.display = "none"
+
+    $("#fScore").html(finalScore.toString().concat("%"));
+    $("#qScore").html(assigQuiz.toString().concat("%"));
+    $("#aScore").html(assigGrade.toString().concat("%"));
+    $("#eScore").html(assigExam.toString().concat("%"));
+    $("#iScore").html(assigIntex.toString().concat("%"));
+    $("#gScore").html(assigGroup.toString().concat("%"));
+    document.getElementById("displayR").style.display = "block"
+});
+
